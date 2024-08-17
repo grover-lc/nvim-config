4c580c261e5173f0410c14c8ae1f88bf414f897f
Grover Lancaster-Cole <glancastercole@me.com>
2024-08-18 08:44:35 +1000

    first commit

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+
+-- vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd.colorscheme "catppuccin-macchiato"
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,16 @@
+return
+{
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {},
+	},
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	}
+}
