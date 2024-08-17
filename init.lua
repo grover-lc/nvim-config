0fb35533e32583e792d5c6b6ce27d9e2ff4df3e9
Grover Lancaster-Cole <glancastercole@me.com>
2024-08-18 09:28:35 +1000

    fix cursor

--- init.lua
+++ init.lua
@@ -2,3 +2,9 @@ require("config.lazy")
 
 -- vim.cmd[[colorscheme tokyonight-night]]
 vim.cmd.colorscheme "catppuccin-macchiato"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.guicursor = "i:hor5"
