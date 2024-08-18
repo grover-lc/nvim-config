5e0468a919d311ef08bc684fd553d9f72b327ab1
Grover Lancaster-Cole <glancastercole@me.com>
2024-08-19 00:06:53 +1000

    fix cursor on leave nvim

--- init.lua
+++ init.lua
@@ -8,3 +8,9 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.guicursor = "i:hor5"
+vim.cmd([[
+    augroup RestoreCursorShapeOnExit
+        autocmd!
+        autocmd VimLeave * set guicursor=a:hor5
+    augroup END
+]])
