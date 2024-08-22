02f7dacdde5340d018cb99e41e53e3574850c49f
Grover Lancaster-Cole <glancastercole@me.com>
2024-08-22 18:10:19 +1000

    update cursor

--- init.lua
+++ init.lua
@@ -1,16 +1,18 @@
 require("config.lazy")
 
--- vim.cmd[[colorscheme tokyonight-night]]
 vim.cmd.colorscheme "catppuccin-macchiato"
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.guicursor = "i:hor5"
+-- set vertical bar for insert and command mode
+-- horizontal bar for replace mode
+vim.opt.guicursor = "i-c:ver20,r:hor20"
+-- set vertical bar when leaving neovim
 vim.cmd([[
     augroup RestoreCursorShapeOnExit
         autocmd!
-        autocmd VimLeave * set guicursor=a:hor5
+        autocmd VimLeave * set guicursor=a:ver20
     augroup END
 ]])
